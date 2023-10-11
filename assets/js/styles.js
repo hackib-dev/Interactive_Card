e39c60a037169904e7d01eb95c8924afbb94e785
hackib-dev <hackibkenn@gmail.com>
2023-10-11 07:54:09 +0100

    JS part updated

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,89 +1,105 @@
-// function card(){
-//     var name = document.querySelector('.cardName');
-//     document.querySelector('.card_name').innerHTML = name.value;
+document.querySelector(".cardName").oninput = () => {
+  document.querySelector(".card_name").innerHTML =
+    document.querySelector(".cardName").value;
+};
 
-//     var number = document.querySelector('.cardNumber');
-//     document.querySelector('.card_num').innerHTML = number.value;
-// }
+document.querySelector(".cardNumber").oninput = () => {
+  const cardNumber = document.querySelector(".cardNumber").value;
+  const cardInput = document.querySelector(".cardNumber");
 
-// document.querySelector('.cardName').oninput = () => {
-//     document.querySelector('.card_name').innerHTML = document.querySelector('.cardName').value;
-// }
+  let card = "";
+  for (let i = 0; i < cardNumber.length; i++) {
+    if (i > 0 && i % 4 === 0) {
+      card += " ";
+    }
+    card += cardNumber[i];
+  }
 
-// document.querySelector('.cardNumber').oninput = () => {
-//     document.querySelector('.card_num').innerHTML = document.querySelector('.cardNumber').value;
-// } 
+  if (card.length === 19) {
+    cardInput.addEventListener("keypress", (e) => {
+      e.preventDefault();
+    });
+  }
 
-// function card(){
-//     var name = document.getElementById('cardName').value;
-//     var number = document.getElementById('cardNumber').value;
-//     var month = document.getElementById('month').value;
-//     var year = document.getElementById('year').value;
-//     var cvc = document.getElementById('cvc').value;
+  document.querySelector(".card_num").innerHTML = card;
+};
 
-//     document.getElementById('card_name').innerHTML = name;
-//     document.getElementById('card_no').innerHTML = number;
-//     document.getElementById('card_date').innerHTML = month;
-//     document.getElementById('card_date').innerHTML = year;
+function cardMonth() {
+  var month = document.getElementById("month").value;
+  document.getElementById("card_month").innerHTML = month;
+}
 
+function cardYear() {
+  var year = document.getElementById("year").value;
+  document.getElementById("card_year").innerHTML = year;
+}
 
-// }
+function cardCvc() {
+  var cvc = document.getElementById("cvc").value;
+  document.getElementById("card_pin").innerHTML = cvc;
+}
 
-// function cardName(){
-//     var name = document.getElementById('cardName').value;
-//     document.getElementById('card_name').innerHTML = name;
-    
-// }   
+function card() {
+  var name = document.querySelector(".cardName").value;
+  var number = document.querySelector(".cardNumber").value;
+  var month = document.querySelector(".month").value;
+  var year = document.querySelector(".year").value;
+  var cvc = document.getElementById("cvc").value;
 
-// function cardNumber(){
-//     var number = document.getElementById('cardNumber').value;
-//     document.getElementById('card_no').innerHTML = number;
+  if (name == "") {
+    nameError = document.querySelector(".name_error");
+    nameError.style.color = "red";
+    nameError.style.fontSize = "xx-small";
+    nameError.style.float = "left";
+    nameError.innerHTML = "Card Name cannot be empty";
+  } else if (name != "") {
+    document.querySelector(".name_error").innerHTML = "";
+  }
 
-// }
+  if (number !== "" && number.match(/^[0-9]+$/) !== null) {
+    document.querySelector(".num_error").innerHTML = "";
+  } else {
+    numError = document.querySelector(".num_error");
+    numError.style.color = "red";
+    numError.style.fontSize = "xx-small";
+    numError.style.float = "left";
+    numError.innerHTML = "Wrong format, numbers only";
+  }
 
-// function cardMonth(){
-//     var month = document.getElementById('month').value;
-//     document.getElementById('card_month').innerHTML = month;
+  if (month == "") {
+    monthError = document.querySelector(".mon_error");
+    monthError.style.color = "red";
+    monthError.style.fontSize = "xx-small";
+    monthError.style.float = "left";
+    monthError.innerHTML = "Can't be blank";
+  } else if (month != "") {
+    document.querySelector(".mon_error").innerHTML = "";
+  }
 
-// }
+  if (year == "") {
+    yearError = document.querySelector(".year_error");
+    yearError.style.color = "red";
+    yearError.style.fontSize = "xx-small";
+    yearError.style.float = "left";
+    yearError.innerHTML = "Can't be blank";
+  } else if (year != "") {
+    document.querySelector(".year_error").innerHTML = "";
+  }
 
-// function cardYear(){
-//     var year = document.getElementById('year').value;
-//     document.getElementById('card_year').innerHTML = year;
-// }
+  if (cvc == "") {
+    cvcError = document.getElementById("cvc_error");
+    cvcError.style.color = "red";
+    cvcError.style.fontSize = "xx-small";
+    cvcError.style.float = "left";
+    cvcError.innerHTML = "Can't be blank";
+  } else if (cvc != "") {
+    document.querySelector(".cvc_error").innerHTML = "";
+  }
 
-// function cardCvc(){
-//     var cvc = document.getElementById('cvc').value;
-//     document.getElementById('card_pin').innerHTML = cvc;
-
-// }
-
-
-// if (document.querySelector('.cardNumber').value.match(numFormat) ){
-//     document.getElementB('name_error').innerHTML = '';
-// }
-
-// else {
-//     nameError = document.getElementById('num_error');
-//     nameError.style.color = "red";
-//     nameError.style.fontSize = "xx-small";
-//     nameError.innerHTML = 'Wrong format, numbers only';
-// }
-
-// if (document.getElementById('month').value == ''){
-//     nameError = document.getElementById('mon_error');
-//     nameError.style.color = "red";
-//     nameError.style.fontSize = "xx-small";
-//     nameError.innerHTML = "Can't be blank";
-// }
-// else if(document.getElementById('month') != ''){
-//     document.getElementB('name_error').innerHTML = '';
-// }
-
-
-
-// if (document.querySelector('.cardName') = ''){
-//     document.getElementById('name_error').style.innerHTML = 'Card Name cannot be empty';
-    
-    
-// }
+  if (name && number && month && year && cvc != "") {
+    var right = document.querySelector(".right");
+    right.style.display = "none";
+    var thank_you = document.querySelector(".thank_you");
+    thank_you.style.display = "block";
+  }
+}
